c74ed59a1f923f02a5355924b68699e92056091e
Lolwa-Coder <champianchampian@gmail.com>
2022-05-31 01:27:48 +0530

    something good4

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,29 @@
 import '../styles/globals.css'
 import { Toolbar } from '../components/toolbar';
 import {ToolbarNews} from '../components/toolbarnews';
+import {AnimatePresence, motion} from 'framer-motion/dist/framer-motion'
 
-function MyApp({ Component, pageProps }) {
-  return <><Toolbar /> <ToolbarNews /><Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return <><Toolbar /> <ToolbarNews />
+ <AnimatePresence>
+    <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      },
+      pageExit: {
+        
+        filter: [
+          'hue-rotate(0) sepia(100%)',
+          'hue-rotate(360deg) sepia(0%)',
+          'hue-rotate(45deg) sepia(100%)',
+          'hue-rotate(0) contrast(100%)',],
+        opacity: 0
+      }
+    }}>
+  <Component {...pageProps} /></motion.div></AnimatePresence>
   </>
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
 import Head from 'next/head'
-
+import {AnimatePresence, motion} from 'framer-motion/dist/framer-motion'
 import styles from '../styles/Home.module.css'
 import {Toolbar} from '../components/toolbar';
 import { Canvas } from "@react-three/fiber";
 //import { motion } from 'framer-motion';
-export default function Home() {
+export default function Home(router) {
   return (
+
     <div className="page-container">
-      
+    <AnimatePresence>
+    <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      },
+      pageExit: {
+        backgroundColor: 'white',
+        filter: [
+          'hue-rotate(0) sepia(100%)',
+          'hue-rotate(360deg) sepia(0%)',
+          'hue-rotate(45deg) sepia(100%)',
+          'hue-rotate(0) contrast(100%)',],
+        opacity: 0
+      }
+    }}>  
      
 
       <div className={styles.main}>
@@ -18,7 +36,7 @@ export default function Home() {
         <h4>one step away from media literacy
           <br /></h4>
       </div>
-     
+     </motion.div></AnimatePresence>
     </div>
   )
 }

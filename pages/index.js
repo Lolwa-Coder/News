bba0deb6dbe8a0deb71608847824d4d0a60895ff
Lolwa-Coder <champianchampian@gmail.com>
2022-05-31 19:02:35 +0530

    something good4

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,7 @@ export default function Home(router) {
   return (
 
     <div className="page-container">
-    <AnimatePresence>
-    <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
-      pageInitial: {
-        opacity: 0
-      },
-      pageAnimate: {
-        opacity: 1
-      },
-      pageExit: {
-        backgroundColor: 'white',
-        filter: [
-          'hue-rotate(0) sepia(100%)',
-          'hue-rotate(360deg) sepia(0%)',
-          'hue-rotate(45deg) sepia(100%)',
-          'hue-rotate(0) contrast(100%)',],
-        opacity: 0
-      }
-    }}>  
+    
      
 
       <div className={styles.main}>
@@ -36,7 +19,7 @@ export default function Home(router) {
         <h4>one step away from media literacy
           <br /></h4>
       </div>
-     </motion.div></AnimatePresence>
+     
     </div>
   )
 }
